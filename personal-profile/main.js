577a5ce7a9b348cce164ce2a491e3bb73174ce95
Dylanwhooooo <140952186+Dylanwhooooo@users.noreply.github.com>
2025-02-13 16:28:36 +1100

    Add files via upload

--- personal-profile/main.js
+++ personal-profile/main.js
@@ -32,6 +32,25 @@ colourThemes.forEach((themeOption) => {
     applyTheme(themeOption.id); // Apply the selected theme
   });
 });
+window.addEventListener("load", loadTheme);
 
 // Load the saved theme when the page loads
-window.addEventListener("load", loadTheme);
+const dvdLogo = document.querySelector("svg");
+const colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+
+function loop() {
+  const hRange = window.innerWidth - dvdLogo.clientWidth;
+  const vRange = window.innerHeight - dvdLogo.clientHeight;
+  const time = performance.now() * 0.2;
+  
+  const x = Math.abs((time % (hRange * 2)) - hRange);
+  dvdLogo.style.left = `${x}px`;
+  const y = Math.abs((time % (vRange * 2)) - vRange);
+  dvdLogo.style.top = `${y}px`;
+  
+  const bounces = Math.floor(time / hRange) + Math.floor(time / vRange);
+  dvdLogo.style.fill = colors[bounces % colors.length];
+  
+  requestAnimationFrame(loop);
+}
+loop();
